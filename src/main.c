723e7a6d71d7584250cb6b92c2066511837461c1
Michał Dziuba <mail@michaldziuba.dev>
2024-07-12 23:38:31 +0200

    http: make simple http server

--- src/main.c
+++ src/main.c
@@ -3,6 +3,9 @@
 #include <stdio.h>
 
 #include "net.h"
+#include "http_request.h"
+#include "http_parser.h"
+#include "logger.h"
 
 #define DEFAULT_PORT 8000
 #define DEFAULT_WORKERS 4
@@ -27,8 +30,30 @@ void echo_handler(lxe_connection_t *conn) {
 
     buffer[bytes_read] = '\0';
 
-    printf("Received: %s", buffer);
-    send(conn->fd, buffer, bytes_read, 0);
+    http_request_t *req = http_request_alloc();
+    req->connection = conn;
+
+    int status = http_parse(req, buffer);
+
+    if (status != 0) {
+      log_err("Parsing failure with status: %d", status);
+      lxe_remove_event(&conn->event, conn->fd);
+      close(conn->fd);
+      http_request_free(req);
+      free(conn);
+      return;
+    }
+
+    log_info("{ method: %s, path: %s }", map_method(req->method), req->path);
+    const char *response = "HTTP/1.1 200 OK\r\nContent-Length: 22\r\nContent-Type: text/html\r\n\r\n<h1>Hello world!</h1>\n";
+    send(conn->fd, response, strlen(response), 0);
+    //free(response);
+    lxe_remove_event(&conn->event, conn->fd);
+
+    close(conn->fd);
+
+    http_request_free(req);
+    free(conn);
 }
 
 void acceptor(lxe_connection_t *conn) {
@@ -36,8 +61,11 @@ void acceptor(lxe_connection_t *conn) {
 }
 
 int main() {
+    int port = DEFAULT_PORT;
     lxe_io_t ctx = lxe_init();
-    lxe_listen(&ctx, DEFAULT_PORT, acceptor);
+    lxe_listen(&ctx, port, acceptor);
+    
+    log_info("Server started on port: %d", port);
     lxe_run(&ctx);
 
     return 0;
