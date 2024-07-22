9075c1126085ec054da0cc3865bae1dcec903676
Michał Dziuba <mail@michaldziuba.dev>
2024-07-22 18:12:39 +0200

    cleanup: remove old unused files and tests

--- src/http_request.h
+++ src/http_request.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "htable.h"
 #include "net.h"
 #include "timer.h"
 #include "http_parser.h"
@@ -55,7 +54,6 @@ typedef struct http_raw_header {
 struct http_request_s {
   char *path;
   http_method_t method;
-  htable_t headers;
   char *body;
   size_t body_size;
   lx_http_parser_t parser;
@@ -68,7 +66,6 @@ typedef struct http_request_s http_request_t;
 
 static http_request_t *http_request_alloc() {
   http_request_t *req = malloc(sizeof(http_request_t));
-  req->headers = htable_create();
   req->path = NULL;
   req->body = NULL;
   req->connection = NULL;
@@ -81,7 +78,6 @@ static http_request_t *http_request_alloc() {
 static void http_request_destroy(http_request_t *req) {
   free(req->path);
   free(req->body);
-  htable_destroy(&req->headers);
 }
 
 static void http_request_free(http_request_t *req) {
